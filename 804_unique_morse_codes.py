2244226763b10a3ff95c858354ac8ddaaf3b9d06
Sharma <abhishek.sharma07@sap.com>
2018-06-28 15:50:40 -0700

    Added 804 program

---
+++ 804_unique_morse_codes.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        morse_codes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
+        return len({''.join(morse_codes[ord(letter) - 97] for letter in word) for word in words})
+
+
+s  = Solution()
+print(s.uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]))
