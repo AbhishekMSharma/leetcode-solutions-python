cb0bdc45f3c38dccbf454ed94c0e95be073fd5ce
Sharma <abhishekmsharma@hotmail.com>
2018-06-28 18:08:45 -0700

    Commented 617

--- 617_merge_two_binary_trees.py
+++ 617_merge_two_binary_trees.py
@@ -12,8 +12,8 @@ class Solution(object):
         :type t2: TreeNode
         :rtype: TreeNode
         """
-        traversal_length = min(len(t1), len(t2))
-        return traversal_length
-
-s = Solution()
-print (s.mergeTrees([1,3,2,5], [2,1,3,null,4,null,7]))
+        if not t1 and not t2: return None
+        result = TreeNode ((t1.val if t1 else 0) + (t2.val if t2 else 0))
+        result.left = self.mergeTrees(t1 and t1.left, t2 and t2.left) #if t1 is true, then t1.left, else t1
+        result.right = self.mergeTrees(t1 and t1.right, t2 and t2.right)
+        return result
