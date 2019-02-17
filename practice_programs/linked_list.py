8e54545b553d91c9ab63f496906105d8551d1b5a
Abhishek Manoj Sharma <abhishekmsharma@hotmail.com>
2019-02-16 20:42:09 -0800

    Updated repo

--- practice_programs/linked_list.py
+++ practice_programs/linked_list.py
@@ -49,6 +49,67 @@ class LinkedList:
 				current.next = t.next
 				return
 			current = current.next
+			
+	# delete node at position 8 -> 10 -> 3 -> 4 -> 7
+	def deleteNodeAtPosition(self, position):
+		if position == 0:
+			#current = self.head.next
+			self.head = self.head.next
+			return
+		
+		current_position = 0
+		current = self.head
+		while (current):
+			if current_position+1 == position:
+				#t = current.next
+				current.next = current.next.next
+				return
+			current = current.next
+			current_position += 1
+			
+	def getLength(self):
+		if self.head is None: return 0
+		current = self.head
+		length = 0
+		while(current):
+			length += 1
+			current = current.next
+		return length
+		
+	def getIndexOfNode(self, data):
+		if self.head is None: return -1
+		current = self.head
+		current_position = 0
+		while (current):
+			if current.data == data:
+				return current_position
+			current_position += 1
+			current = current.next
+		return -1
+		
+	def getNodeAtPosition(self, position):
+		if position == 0: return self.head.data
+		current = self.head.next
+		current_position = 1
+		while (current):
+			if current_position == position:
+				return current.data
+			current_position += 1
+			current = current.next
+			
+	# 8 -> 10 -> 3 -> 5 -> 7 -> 2
+	def getNthNodeFromEnd(self, position):
+		linked_list_length = self.getLength()
+		position = linked_list_length - position
+		print ("Position", position)
+		return self.getNodeAtPosition(position)
+		
+	def getMiddleOfLinkedList(self):
+		slow_pointer = fast_pointer = self.head
+		while fast_pointer and fast_pointer.next:
+			fast_pointer = fast_pointer.next.next
+			slow_pointer = slow_pointer.next
+		return slow_pointer.data
 	
 	def printLinkedList(self):
 		print ("Printing Linked List")
@@ -70,3 +131,10 @@ linked_list.printLinkedList()
 linked_list.deleteNode(11)
 linked_list.append(8)
 linked_list.printLinkedList()
+linked_list.deleteNodeAtPosition(3)
+linked_list.printLinkedList()
+print("Linked List Length:",linked_list.getLength())
+print("Linked List get index of data 8: ", linked_list.getIndexOfNode(10))
+print("Linked List get element at index 2: ", linked_list.getNodeAtPosition(2))
+print("Linked List get element at 3rd position from end: ", linked_list.getNthNodeFromEnd(3))
+print("Middle element of Linked List: ", linked_list.getMiddleOfLinkedList())
