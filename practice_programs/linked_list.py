49b0595921da9e2d9ccfb3f5e2b545b878fe6d4f
Abhishek Manoj Sharma <abhishekmsharma@hotmail.com>
2019-02-16 18:44:13 -0800

    Updated repo

--- practice_programs/linked_list.py
+++ practice_programs/linked_list.py
@@ -6,7 +6,8 @@ class Node:
 class LinkedList:
 	def __init__ (self):
 		self.head = None
-		
+	
+	# add new node at the end
 	def append(self, data):
 		if (self.head == None):
 			self.head = Node(data)
@@ -15,6 +16,30 @@ class LinkedList:
 			while (last.next):
 				last = last.next
 			last.next = Node(data)
+			
+	# add new node at the start
+	def prepend(self, data):
+		if (self.head == None):
+			self.head = Node(data)
+			
+		else:
+			temp = self.head
+			self.head = Node(data)
+			self.head.next = temp
+			
+	# insert after values
+	def insertAfterValue(self, value, data):
+		current = self.head
+		while(current):
+			if current.data == value:
+				new_node = Node(data)
+				new_node.next = current.next
+				current.next = new_node
+				return
+			current = current.next
+		
+		# if value not found, appending data at the end
+		self.append(data)
 	
 	def printLinkedList(self):
 		current = self.head
@@ -27,5 +52,8 @@ class LinkedList:
 linked_list = LinkedList()
 linked_list.append(3)
 linked_list.append(4)
+linked_list.prepend(8)
+linked_list.insertAfterValue(8,10)
 linked_list.append(7)
+linked_list.insertAfterValue(7,11)
 linked_list.printLinkedList()
